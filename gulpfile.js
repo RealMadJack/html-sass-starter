b81efedff112ee280f3fc093a03bb08780989e22
Dmitry G <gupalo1108@gmail.com>
2019-03-30 12:04:10 +0300

    package json, initial gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+////////////////////////////////
+// Setup
+////////////////////////////////
+
+// Gulp and package
+const { src, dest, parallel, series, watch } = require('gulp')
+const pjson = require('./package.json')
+
+// Plugins
+const autoprefixer = require('autoprefixer')
+const browserSync = require('browser-sync').create()
+
+const cssnano = require ('cssnano')
+const imagemin = require('gulp-imagemin')
+const pixrem = require('pixrem')
+const plumber = require('gulp-plumber')
+const postcss = require('gulp-postcss')
+const reload = browserSync.reload
+const rename = require('gulp-rename')
+const sass = require('gulp-sass')
+const spawn = require('child_process').spawn
+const uglify = require('gulp-uglify-es').default
